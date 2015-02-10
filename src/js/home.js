dbc51cbab11bbebdc7186f16eec160957beae41b
Kiran D Cunha <krndon@gmail.com>
2015-02-10 12:26:54 +0530

    updated splash screen, added orientation support

--- src/js/home.js
+++ src/js/home.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-	 $('#div').text("Hello World");
+	init();
+	function init() {
+		$('#div').text("Hello World");
+	}
+	function onOrientationChange() {
+		switch (window.orientation) {
+			case -90:
+				$('#div').text('landscape 90');
+				break;
+			case 90:
+				$('#div').text('landscape -90');
+				break;
+			default:
+				$('#div').text('portrait');
+				break;
+		}
+	}
+
+	window.addEventListener('orientationchange', onOrientationChange);
+
 });
